16bcf9cfda0d94107061f8398f2be876ec360ef7
Mcnoor <99060667+Mcnoor@users.noreply.github.com>
2022-07-02 18:19:37 -0500

    updating files

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,10 +1,10 @@
-const router = require('express').Router();
-const categoryRoutes = require('./category-routes');
-const productRoutes = require('./product-routes');
-const tagRoutes = require('./tag-routes');
+const router = require("express").Router();
+const categoryRoute = require("./category-routes");
+const productRoute = require("./product-routes");
+const tagRoute = require("./tag-routes");
 
-router.use('/categories', categoryRoutes);
-router.use('/products', productRoutes);
-router.use('/tags', tagRoutes);
+router.use("/categories", categoryRoute);
+router.use("/products", productRoute);
+router.use("/tags", tagRoute);
 
 module.exports = router;
